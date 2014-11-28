c5aea04c09837025b7dd318d06db25db17b62592
classmateyang <classmateyang@gmail.com>
2014-11-28 17:38:47 -0500

    read and write csv files

--- rdcsv.py
+++ rdcsv.py
@@ -1,1 +1,42 @@
 __author__='yangyang'
+import csv
+import pprint
+
+localfile='./files/budget.csv'
+infile=open(localfile,'r')
+table=[]
+
+for row in csv.reader(infile):
+	table.append(row)
+infile.close()
+
+#pprint.pprint(table)
+'''
+for r in range(1,len(table)):
+	for c in range(1,len(table[0])):
+		table[r][c]=float(table[r][c])
+'''
+#pprint.pprint(table)
+
+for r in table[1:]:
+	for c in r[1:]:
+		table[table.index(r)][r.index(c)]=float(c)
+
+
+row=[0.0]*len(table[0])
+for c in range(1,len(row)):
+	s=0
+	for r in range(1,len(table)):
+		s +=table[r][c]
+	row[c]=s
+row[0]='total'
+
+
+table.append(row)
+
+
+outfile=open('budget11.csv','w')
+writer=csv.writer(outfile)
+for row in table:
+	writer.writerow(row)
+outfile.close()
